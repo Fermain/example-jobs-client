45528a3c3891da384ebe760220683cab4b64184a
Fermain <Fermain@users.noreply.github.com>
2023-06-05 11:28:29 +0200

    Adds API key

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import * as venues from "./venues";
 
-export default {
-  venues
+export default class NoroffJobsAPI {
+  constructor(private apiKey: string) {
+    console.log(apiKey);
+  }
+
+  venues = {
+    get: venues.getVenues
+  }
 }
